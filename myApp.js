f71698932d15c648487eedb79f395a29e9c14dd3
Rudy <rudy.im@outlook.com>
2025-06-03 21:16:30 -0400

    #8

--- myApp.js
+++ myApp.js
@@ -33,7 +33,12 @@ app.get('/json', (req, res) => {
 });
 
 
-
+app.get('/now', function(req, res, next) {
+	req.time = new Date().toString();
+	next();
+  }, function(req, res) {
+	res.send({time: req.time});
+  });
 
 
 
